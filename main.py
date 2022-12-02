e48d67ca78c1f62906e13d486cc512d91176dcdb
Konstantin <sunprizrak@gmail.com>
2022-12-03 00:16:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from just_generate import JustGenerate
+
+Window.size = (360, 600)
+
+
+class MainApp(MDApp):
+    pass
+
+
+if __name__ == '__main__':
+    MainApp().run()
