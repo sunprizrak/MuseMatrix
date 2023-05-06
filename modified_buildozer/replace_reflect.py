fb9ce7862ccb12a0d5fc1fb6ab46ccefdf67607d
sunprizrak <sunprizrak@gmail.com>
2023-05-06 17:05:59 +0300

    ...

--- modified_buildozer/replace_reflect.py
+++ modified_buildozer/replace_reflect.py
@@ -4,7 +4,7 @@ import os
 package_name = (
     [
         line
-        for line in open(f"{os.getcwd()}/buildozer.spec", "r").readlines()
+        for line in open(f"../buildozer.spec", "r").readlines()
         if line.startswith("package.name")
     ][0]
     .split("=")[1]
@@ -12,12 +12,12 @@ package_name = (
 )
 
 files_to_replace = [
-    f"{os.getcwd()}/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/python-installs/{package_name}/arm64-v8a/jnius/reflect.py"
+    f"../.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/python-installs/{package_name}/arm64-v8a/jnius/reflect.py"
 ]
 
 fixed_files = [
-    file for file in os.listdir(f'{os.getcwd()}/modified_buildozer') if file == 'reflect.py'
+    file for file in os.listdir('../modified_buildozer') if file == 'reflect.py'
 ]
 
 for file in fixed_files:
-    os.system(f"cp {os.getcwd()}/modified_buildozer/{file} {files_to_replace[0]}")
+    os.system(f"cp {file} {files_to_replace[0]}")
